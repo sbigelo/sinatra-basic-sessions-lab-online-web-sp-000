70bdf2b6bf19089f79f665d8817fce862dad0e0e
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-14 21:05:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,15 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
+
   get '/' do
     erb :index
+  end
+
+  post '/checkout' do
+    session[item] = "item"
+    @session = session
+    session
   end
 
 end
