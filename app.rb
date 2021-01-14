66ba9dd494c22e23132edf4d6e997acecb8bcd0f
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-14 20:42:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do 
+    enable :sessions
+    set :session_secret, "secret"
+
+  # get '/' do
+  #   erb :form
+  # end
+  
 end
